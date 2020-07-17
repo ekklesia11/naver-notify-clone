f8e6fb30f4ed79dac4f6ac24ae5522c639fb627e
Daniel Park <ekklesia89@gmail.com>
2020-07-17 23:56:04 +0900

    global style added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import React from "react";
+import { createGlobalStyle } from "styled-components";
 
 function App() {
-  return <div>naver 내 서랍 clone</div>;
+  return (
+    <div>
+      <GlobalStyle />
+      <div style={{ color: "#fff" }}>내 서랍</div>
+    </div>
+  );
 }
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    background-color: rgb(77, 54, 173);
+  }
+`;
+
+// const Container = styled.div`
+//   background-color: rgb(77, 54, 173);
+// `;
 
 export default App;
